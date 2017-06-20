97bff12b5648eb42ac0c9acd962d1ba50e31ef4f
Hunter L. Allen <hunter@openrobotics.org>
2017-06-19 23:48:28 -0700

    Port rosserial_python. Todo: port the actual nodes (install is ported).

--- rosserial_python/setup.py
+++ rosserial_python/setup.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['rosserial_python'],
-    package_dir={'': 'src'},
-    )
+setup(
+    name='rosserial_python',
+    version='0.7.6',
+    packages=[],
+    py_modules=[
+        'nodes.message_info_service', 'nodes.serial_node',
+        'src.rosserial_python.SerialClient'],
+    install_requires=['setuptools'],
+    author='Michael Ferguson',
+    maintainer='Hunter L. Allen',
+    maintainer_email='hunter@openrobotics.org',
+    keyword=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'A Python-based implementation of the ROS serial protocol.'
+    ),
+    license='BSD',
+    # todo: test_suite=test
+    entry_points={
+        'console_scripts': [
+            'message_info_service = nodes.message_info_service:main',
+            'serial_node = nodes.serial_node:main'
+        ]
+    },
+)
 
-setup(**d)
+# d = generate_distutils_setup(
+#     packages=['rosserial_python'],
+#     package_dir={'': 'src'},
+#     )
+#
+# setup(**d)
